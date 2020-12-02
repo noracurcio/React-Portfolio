eede23b2c4fbfdc19171c9d61e8575ab56099418
noracurcio <noracurcio@gmail.com>
2020-12-01 17:12:50 -0700

    still trying to fix.

--- server.js
+++ server.js
@@ -50,20 +50,9 @@ router.post('/send', (req, res, next) => {
   })
 })
 
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use('/', router)
+app.listen(PORT)
 
-
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-// Add routes, both API and view
-app.use(routes);
-
-
-// Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-});
