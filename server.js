094626cd1d9d5b58a27e43977fe440833b905da0
noracurcio <noracurcio@gmail.com>
2020-12-01 17:47:01 -0700

    fixing still.

--- server.js
+++ server.js
@@ -50,13 +50,18 @@ router.post('/send', (req, res, next) => {
   })
 })
 
-app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/index.html"));
-  });
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/', router)
 app.listen(PORT)
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/index.html"));
+  });
+
 
