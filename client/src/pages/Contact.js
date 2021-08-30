ec985e185db481e1f00abd4594ac6cbfc5ddc033
noracurcio <noracurcio@gmail.com>
2021-08-30 16:28:33 -0600

    fixing contact page.

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -36,51 +36,11 @@ class Contact extends React.Component {
       <>
         <h1>Contact Nora</h1>
         <div className="row">
-          <div className="container col-4 home-container">
+          <div className="container col-3 home-container">
             <br></br>
-
-            <div className="App">
-              <form
-                id="contact-form"
-                onSubmit={this.handleSubmit.bind(this)}
-                method="POST"
-              >
-                <div className="form-group">
-                  <label htmlFor="name">Name</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="name"
-                    value={this.state.name}
-                    onChange={this.onNameChange.bind(this)}
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="exampleInputEmail1">Email address</label>
-                  <input
-                    type="email"
-                    className="form-control"
-                    id="email"
-                    aria-describedby="emailHelp"
-                    value={this.state.email}
-                    onChange={this.onEmailChange.bind(this)}
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="message">Message</label>
-                  <textarea
-                    className="form-control"
-                    rows="5"
-                    id="message"
-                    value={this.state.message}
-                    onChange={this.onMessageChange.bind(this)}
-                  />
-                </div>
-                <button type="submit" className="btn btn-primary">
-                  Submit
-                </button>
-              </form>
-            </div>
+            <p className="contact-p">
+              Phone: 720-369-6073 | Email: noracurcio@gmail.com
+            </p>
           </div>
         </div>
       </>
